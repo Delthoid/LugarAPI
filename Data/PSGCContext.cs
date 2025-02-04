4272413dd5545fac53f6c61ee218fc63ab670bbf
Delthon <delthonvillanueva20@gmail.com>
2025-02-04 19:10:08 +0800

    Context

---
+++ Data/PSGCContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LugarAPI.Data
+{
+    public class PSGCContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=MyServer; Database=MyDatabase; Trusted_Connection=True;");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
+using LugarAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<PSGCContext>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
