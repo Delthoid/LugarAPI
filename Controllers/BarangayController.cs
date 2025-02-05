397f5c560c2bfd9ac6e69f9eeb0307baea311bdf
Delthon <delthonvillanueva20@gmail.com>
2025-02-05 16:58:47 +0800

    Get Barangays

---
+++ Controllers/BarangayController.cs
@@ -0,0 +1,36 @@
+﻿using LugarAPI.Model;
+using LugarAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
+
+namespace LugarAPI.Controllers
+{
+    [EnableRateLimiting("FixedPolicy")]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BarangayController : Controller
+    {
+        private readonly LugarService _service;
+
+        public BarangayController(LugarService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public Result<Barangay> Get([FromQuery] int page = 1, [FromQuery] int limit = 100, [FromQuery] string query = "")
+        {
+            if (page < 1) page = 1;
+
+            var barangays = _service.GetBarangays(page, limit, query);
+            var result = new Result<Barangay>()
+            {
+                Message = "Get Barangays",
+                Total = barangays.Count,
+                Data = barangays ?? [],
+            };
+
+            return result;
+        }
+    }
+}
